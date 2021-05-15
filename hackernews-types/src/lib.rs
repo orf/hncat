19a946b252c0b04a31ffa4c933846d326f38a01b
Tom Forbes <tom@tomforb.es>
2021-05-15 13:49:18 +0100

    Fix timestamp deserialization

--- hackernews-types/src/lib.rs
+++ hackernews-types/src/lib.rs
@@ -8,7 +8,7 @@ pub enum Item {
     DeletedItem {
         id: u32,
         deleted: bool,
-        #[serde(deserialize_with = "ts_seconds::deserialize")]
+        #[serde(with = "ts_seconds")]
         time: DateTime<Utc>,
         #[serde(rename = "type")]
         type_: String,
@@ -18,7 +18,7 @@ pub enum Item {
         #[serde(default)]
         dead: bool,
         id: u32,
-        #[serde(deserialize_with = "ts_seconds::deserialize")]
+        #[serde(with = "ts_seconds")]
         time: DateTime<Utc>,
         #[serde(flatten)]
         variant: ItemVariant,
@@ -66,4 +66,28 @@ pub enum ItemVariant {
         score: i32,
         text: String,
     },
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Item;
+    use matches::assert_matches;
+    use serde_json;
+
+    #[test]
+    fn test_simple_parse() {
+        let json = r#"
+        {
+          "by": "gzer0",
+          "dead": false,
+          "id": 27164354,
+          "time": 1620568471,
+          "type": "comment",
+          "kids": [],
+          "parent": 27164095,
+          "text": "A message goes here"
+        }
+        "#;
+        assert_matches!(serde_json::from_str(json), Ok(Item::LiveItem { .. }))
+    }
 }
